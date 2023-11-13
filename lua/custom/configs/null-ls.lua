8a3dad43f3dce5e14899456c15e99b7bfa57f500
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2023-11-13 21:50:22 +0200

    update null ls

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,7 +1,6 @@
 local null_ls = require "null-ls"
 
 local b = null_ls.builtins
-local u = null_ls.utils
 
 local sources = {
 
@@ -13,26 +12,24 @@ local sources = {
 
   -- cpp
   b.formatting.clang_format,
+
   b.diagnostics.eslint_d.with({
-    filetypes = { "javascript", "typescript", "vue", "html", "css" },
     condition = function()
-      return u.root_pattern(
+      return require"null-ls.utils".root_pattern(
         "eslint.config.js",
-        -- https://eslint.org/docs/user-guide/configuring/configuration-files#configuration-file-formats
         ".eslintrc",
         ".eslintrc.js",
         ".eslintrc.cjs",
         ".eslintrc.yaml",
         ".eslintrc.yml",
-        ".eslintrc.json",
-        "package.json"
+        ".eslintrc.json"
       )(vim.api.nvim_buf_get_name(0)) ~= nil
-    end,
+    end
   }),
+
   b.formatting.prettierd.with({
-    -- use prettier only with prettierrc present
     condition = function()
-      return u.root_pattern(
+      return require"null-ls.utils".root_pattern(
         ".prettierrc",
         ".prettierrc.json",
         ".prettierrc.yml",
@@ -44,7 +41,7 @@ local sources = {
         "prettier.config.js",
         "prettier.config.cjs"
       )(vim.api.nvim_buf_get_name(0)) ~= nil
-    end,
+    end
   }),
 }
 
