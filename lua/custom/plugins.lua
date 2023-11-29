80c007ccfc2267dff8ef38bc9b3aef259e336570
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2023-11-29 11:09:42 +0200

    add lsp file configuration

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -89,6 +89,16 @@ local plugins = {
       require "custom.configs.snip"
     end,
   },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
 
 return plugins
