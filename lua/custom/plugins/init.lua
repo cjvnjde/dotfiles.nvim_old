04b9e93c346bcd66a1a197b0f06e69cb73856d40
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-01-27 17:48:11 +0200

    Update init.lua and plugins
    The commit updates the init.lua file of the project. It makes changes to the leader key, adds a useful plugin for keybinds, and removes commented code related to gitsigns.nvim.
    
    Additionally, the commit adds a new file named lazy-lock.json. This file contains information about the specific versions and commits of various plugins used in the project.
    
    The "custom/plugins/init.lua" file is also modified. The change adds a new plugin for a file explorer (nvim-tree.nvim) and configures it to toggle and focus the file explorer using specific key mappings.
    
    Overall, these modifications improve the configuration and functionality of the Neovim setup.
    
    Commit: Update init.lua and plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,61 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+{
+    'nvim-tree/nvim-tree.lua',
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    init = function()
+      vim.keymap.set('n', '<C-b>', '<cmd> NvimTreeToggle <CR>', { desc = 'Toggle nvimtree' })
+      vim.keymap.set('n', '<leader>.', '<cmd> NvimTreeFocus <CR>', { desc = 'Focus nvimtree' })
+    end,
+    opts = function()
+      return {
+        filters = {
+          dotfiles = false,
+        },
+        disable_netrw = true,
+        hijack_netrw = true,
+        hijack_cursor = true,
+        hijack_unnamed_buffer_when_opening = false,
+        sync_root_with_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = false,
+        },
+        view = {
+          adaptive_size = true,
+          side = "left",
+          width = 30,
+          preserve_window_proportions = true,
+        },
+        git = {
+          enable = false,
+          ignore = true,
+        },
+        filesystem_watchers = {
+          enable = true,
+        },
+        actions = {
+          open_file = {
+            resize_window = true,
+          },
+        },
+        renderer = {
+          root_folder_label = false,
+          highlight_git = false,
+          highlight_opened_files = "none",
+
+          indent_markers = {
+            enable = false,
+          },
+        },
+      }
+    end,
+  }
+}
